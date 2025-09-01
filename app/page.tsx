dd2183aa4ca2b9259b2aaeade7e190a853807c53
Mehul Rana <86463576+mehulrana017@users.noreply.github.com>
2025-09-02 01:34:19 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
       {/* <CommunitySection /> */}
       {/* <PricingSection /> */}
       <ContactSection />
-      <FAQSection />
+      {/* <FAQSection /> */}
       <FooterSection />
     </>
   );
